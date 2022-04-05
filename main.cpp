045d598b804859cf42bc285838f986e437d4eed7
Victor Raposo <victor.ravaglia@hotmail.com>
2022-04-05 18:13:46 -0300

    Criada tabela de  simbolos

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <math.h>
 #include <algorithm>
 #include <iterator>
 #include <unistd.h>
@@ -97,49 +98,101 @@ vector<vector<pair<string, float>>> createHuffmanTable(const map<char, float> &s
 
 map<char, string> createSymbolTable(vector<vector<pair<string, float>>> &huffmanTree){
     map<char, string> symbolCodes;
-    char test = 'A';
-    string code;
-    bool endCode = false;
-    vector<vector<pair<string, float>>>::size_type idxColumn = huffmanTree.size();
 
-    while (!endCode)
+    for (auto& s : huffmanTree[0])
     {
-        for (auto& symbols : huffmanTree[idxColumn])
-        {
-            string::size_type idx = symbols.first.find(test);
-            if (idx != string::npos)
-            {
-                cout << symbols.first << " | " << idx << "\n";
-                if(symbols.first.size() == 1)
-                {
-                    endCode = true;
-                }
-            }
-        }
-        idxColumn--;
-    }
+        char charTest = s.first[0];
+        string code;
+        bool endCode = false;
+        vector<vector<pair<string, float>>>::size_type idxColumn = huffmanTree.size();
 
-    cout << "Codigo: " << code << "\n";
+        while (!endCode)
+        {
+            int idxSymbol = 1;
+            for (auto& symbols : huffmanTree[idxColumn])
+            {
+                string::size_type idx = symbols.first.find(charTest);
+
+                if (idx != string::npos)
+                {
+//                    cout << symbols.first << " | " << idxSymbol << " | " << huffmanTree[idxColumn].size() <<"\n";
+                    if(symbols.first.size() == 1)
+                    {
+                        endCode = true;
+                    }
+                    int codeToAppend = 1 - (huffmanTree[idxColumn].size() - idxSymbol);
+                    if(codeToAppend >= 0){
+                        code += to_string(codeToAppend);
+                    }
+
+                }
+                idxSymbol++;
+            }
+            idxColumn--;
+        }
+
+        cout << "Simbolo: " << int(charTest) << " | Codigo: " << code << "\n";
+
+        symbolCodes.insert(pair<char, string>(charTest, code));
+    }
 
     return symbolCodes;
 }
 
-void encoder(const string& filename){
+float calculateEntropy(map<char, float> &symbolProbabilities){
+    float H = 0;
 
+    for(auto& symbol : symbolProbabilities){
+        H -= symbol.second*log2(symbol.second);
+    }
+
+    return H;
+}
+
+float calculateBits(map<char, string> &symbolTable, map<char, float> &symbolProbabilities){
+    float Bits = 0;
+
+    for(auto& symbol : symbolTable){
+        for(auto& symbol2 : symbolProbabilities){
+            if(symbol.first == symbol2.first){
+                Bits += float(symbol.second.size())*symbol2.second;
+            }
+        }
+    }
+
+    return Bits;
+}
+
+void encoder(const string& filename, map<char, string> &symbolTable){
+
+
+}
+
+map<char, string> sourceAnalysis(const string& filename){
     float totalCount = 0;
     map<char, int> symbolCount = generateSymbols(filename, totalCount);
     map<char, float> symbolProbabilities = calculateProbabilities(symbolCount, totalCount);
     vector<vector<pair<string, float>>> huffmanTree = createHuffmanTable(symbolProbabilities);
     map<char, string> symbolTable = createSymbolTable(huffmanTree);
 
+    float H = calculateEntropy(symbolProbabilities);
+    float bitsPerSymbol = calculateBits(symbolTable, symbolProbabilities);
+
+    cout << "\n\nEntropia da fonte: " << H << "\n";
+    cout << "Bits/s apos Huffman: " << bitsPerSymbol << "\n";
+    cout << "Redundancia: " << bitsPerSymbol - H << "\n";
+
 //    cout << "\n\n Final: \n";
 //    for(const auto& elem : huffmanTree.back())
 //    {
 //        cout << elem.first << " " << elem.second << "\n";
 //    }
+
+    return  symbolTable;
 }
 
 int main() {
-    encoder("./../biblia.txt");
+    map<char, string> symbolTable = sourceAnalysis("./../biblia.txt");
+    encoder("./../biblia.txt", symbolTable);
     return 0;
 }
